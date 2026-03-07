c214c99d3b12ef5b858dc0e5258e3f1b91414edb
Masud <wubetumasud@gmail.com>
2026-03-07 09:20:39 +0300

    product create route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,7 +9,7 @@ const productRouter = express.Router();
 
 //Web frontend routes
 productRouter.get("/new", showCreateForm);
-// productRouter.post("/", createProduct);
+productRouter.post("/", createProduct);
 // productRouter.get("/", getProducts);
 // productRouter.get("/:id", getProduct);
 
